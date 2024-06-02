dff350bbbeedd536df70bbf402d179bbd7114462
Remsely <prokopchukromain@mail.ru>
2024-06-02 19:11:00 +0300

    fix: справить размер столбцов в таблице posts

--- back/src/main/java/edu/mirea/remselybokgosha/start/post/entity/Post.java
+++ back/src/main/java/edu/mirea/remselybokgosha/start/post/entity/Post.java
@@ -20,10 +20,10 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 100)
     private String title;
 
-    @Column(nullable = false, length=10000)
+    @Column(nullable = false, length = 3000)
     private String content;
 
     @Column(nullable = false)
